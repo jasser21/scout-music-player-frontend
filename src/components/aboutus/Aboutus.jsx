9c4087d9858c128fd76b8bff496768107f169424
jasser21 <jesuisjasser@gmail.com>
2023-11-21 12:25:23 +0100

    basic changes

--- src/components/aboutus/Aboutus.jsx
+++ src/components/aboutus/Aboutus.jsx
@@ -1,7 +1,75 @@
 import React from "react";
-
+import "./aboutus.css";
 const Aboutus = () => {
-  return <div>Aboutus</div>;
+  return (
+    <div className="aboutus">
+      <h1>Scouting we go, exploring the world with joy and glow</h1>
+      <p>
+        Welcome to our Tunisian Scout Music website! We are a team of music
+        enthusiasts who are passionate about promoting and preserving Tunisian
+        scout music.
+      </p>
+      <p>
+        Our mission is to make Tunisian scout music more accessible to people
+        around the world. We believe that this music is not only an important
+        part of Tunisian culture, but also a valuable source of inspiration and
+        education for people of all ages and backgrounds.
+      </p>
+      <p>
+        As Tunisian scout music enthusiasts, we have spent countless hours
+        researching and collecting some of the best and most inspiring songs
+        from the Tunisian scout music genre. We have created a website that
+        features a collection of albums and lyrics, making it easy for visitors
+        to find and enjoy their favorite songs.
+      </p>
+      <p>
+        We are committed to providing our visitors with the best possible
+        experience. If you have any questions or feedback, please do not
+        hesitate to contact us.
+      </p>
+      <p>
+        Thank you for visiting our website and supporting Tunisian scout music!
+      </p>
+    </div>
+  );
 };
 
 export default Aboutus;
+
+//         <style>
+//             h1  {
+//                 margin: 90px;
+//             }
+//             p{
+
+//                 color: rgba(228, 228, 228, 0.758);
+//                 margin: auto 150px;
+//                 padding: 5px;
+//                 font-size: 25px;
+//                 line-height: 35px;
+//                 text-indent: 50px;
+//             }
+//             p {
+//   color:rgba(228, 228, 228, 0.758);;
+//   animation: color-change 10s ease-in-out ;
+// }
+
+// @keyframes color-change {
+//   0% {
+//     color: rgba(228, 228, 228, 0.758);
+//   }
+//   5% {
+//     color: #626a53a1;
+//   }
+//   10% {
+//     color: #5de2a0a9;
+//   }
+//   15% {
+//     color: rgba(228, 228, 228, 0.758);
+//   }
+//   38%{
+//     color:white ;
+//   }
+// }
+
+//         </style>
